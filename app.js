b13a6a24182fcdd0c0e327b804db71b4cf200744
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-10-27 10:15:43 +0100

    Add custom error handler and other middleware

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use("/books", bookRouter);
 app.use("/authors", authorRouter);
 app.use("/", indexRouter);
 
+// Every thrown error in the application or the previous middleware function calling `next`
+// with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and
+  // if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
+
 const PORT = 3000;
 
 app.listen(PORT, (error) => {
---
+++ controllers/authorControllers.js
@@ -0,0 +1,14 @@
+const db = require("../db");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
+
+async function getAuthorById(req, res) {
+  const { authorId } = req.params;
+  const author = await db.getAuthorById(Number(authorId));
+
+  if (!author) {
+    throw new CustomNotFoundError("Author not found");
+  }
+  res.send(`Author Name: ${author.name}`);
+}
+
+module.exports = { getAuthorById };
